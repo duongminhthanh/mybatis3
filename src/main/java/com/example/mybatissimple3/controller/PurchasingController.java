f56b3f48375be75b4404ee680e124365e5e42bf4
duongminhthanh <62210826+duongminhthanh@users.noreply.github.com>
2022-03-11 09:27:30 +0700

    Add files via upload

--- src/main/java/com/example/mybatissimple3/controller/PurchasingController.java
+++ src/main/java/com/example/mybatissimple3/controller/PurchasingController.java
@@ -1,6 +1,5 @@
 package com.example.mybatissimple3.controller;
 
-import com.example.mybatissimple3.model.Products;
 import com.example.mybatissimple3.model.Purchasing;
 import com.example.mybatissimple3.service.PurchasingService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,12 +15,37 @@ public class PurchasingController {
     @RequestMapping(value = "/purchasing/insert",method = RequestMethod.POST)
     @ResponseBody
     public int insertPurchasing(@RequestBody Purchasing purchasing){
-        return purchasingService.insertPurchasing(purchasing);
+        return purchasingService.insert(purchasing);
     }
     /*list all*/
     @RequestMapping(value = "/purchasing",method = RequestMethod.GET)
     @ResponseBody
     public List<Purchasing> selectAllPurchasing(){
         return purchasingService.selectAllPurchasing();
+    }
+    /*update*/
+    @RequestMapping(value="/purchasing/update/{id}",method = RequestMethod.PUT)
+    @ResponseBody
+    public int updatePurchaisngById(@PathVariable int id){
+        return purchasingService.updatePurchasingById(id);
+    }
+    /*delete*/
+    @RequestMapping(value="/purchasing/delete/{id}",method = RequestMethod.DELETE)
+    @ResponseBody
+    public int deletePurchaisngById(@PathVariable int id){
+        return purchasingService.deletePurchasingById(id);
+    }
+    /*list by id*/
+    @RequestMapping(value="/purchasing/{id}",method = RequestMethod.GET)
+    @ResponseBody
+    public Purchasing selectPurchaisngById(@PathVariable int id){
+        return purchasingService.selectPurchasingById(id);
+    }
+
+    /*count all*/
+    @RequestMapping(value = "/purchasing/count",method = RequestMethod.GET)
+    @ResponseBody
+    public int countAll(){
+        return purchasingService.countAll();
     }
 }
