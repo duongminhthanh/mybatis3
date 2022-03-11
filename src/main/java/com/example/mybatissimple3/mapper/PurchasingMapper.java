961aa280272c37f63a83bc344a775ecb3bbd7d33
duongminhthanh <62210826+duongminhthanh@users.noreply.github.com>
2022-03-11 09:28:04 +0700

    Add files via upload

--- src/main/java/com/example/mybatissimple3/mapper/PurchasingMapper.java
+++ src/main/java/com/example/mybatissimple3/mapper/PurchasingMapper.java
@@ -1,8 +1,8 @@
 package com.example.mybatissimple3.mapper;
 
-import com.example.mybatissimple3.model.Products;
 import com.example.mybatissimple3.model.Purchasing;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,6 +10,10 @@ import java.util.List;
 @Mapper
 @Service
 public interface PurchasingMapper {
-    public int insertPurchasing(Purchasing purchasing);
+    public int insert(Purchasing purchasing);
     public List<Purchasing> selectAllPurchasing();
+    public int updatePurchasingById(int id);
+    public int deletePurchasingById(int id);
+    public Purchasing selectPurchasingById(int id);
+    public int countAll();
 }
